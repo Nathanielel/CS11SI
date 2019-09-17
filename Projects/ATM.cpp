3866adbfa708d536f3d6b0470cd363c27065600b
Nathanielel <44421830+Nathanielel@users.noreply.github.com>
2019-09-17 15:49:23 -0700

    Update ATM.cpp

--- Projects/ATM.cpp
+++ Projects/ATM.cpp
@@ -1,8 +1,24 @@
+/**
+    A secure ATM program that will let us adjust and check our balance.
+    ATM CAN: Withdraw Funds, Deposit Funds, Display Balance
+*/
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-int main(){
 
+const string WELCOME = "Hello and welcome to ";
+const sting BANK_NAME = "Your Bank Name Here";
+const int PIN = 0000;
+const double balance = 122.58;
+
+int main(){
+  cout << WELCOME << BANK_NAME << endl << "Please enter your PIN";
+  //get pin
+  //check pin
+  //present menu
+  //get selection
+  //perform appropriate operation
   return 0;                                                                                                           }                                                                                                                     
 }
